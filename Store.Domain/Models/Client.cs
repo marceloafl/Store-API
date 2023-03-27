87b016687ee13a7f3514f65f64ab05ff1f0e0ab6
Marcelo Lemes <marceloaflemes@gmail.com>
2023-03-27 16:34:29 -0300

    starting project and creating models Purchase and Client

---
+++ Store.Domain/Models/Client.cs
@@ -0,0 +1,15 @@
+﻿namespace Store.Domain.Models
+{
+    public class Client
+    {
+        public string ClientId { get; set; }
+        public string ClientName { get; set; }
+        public string ClientCPF { get; set; }
+        public string ClientContry { get; set; }
+        public string ClientState { get; set; }
+        public string ClientCity { get; set; }
+        public string ClientAddress { get; set; }
+        public DateTime ClientBirth { get; set; }
+        public ICollection<Purchase> Purchases { get; set; }
+    }
+}
---
+++ Store.Domain/Models/Purchase.cs
@@ -0,0 +1,12 @@
+﻿namespace Store.Domain.Models
+{
+    public class Purchase
+    {
+        public string PurchaseId { get; set; }
+        public string ClienteId { get; set; }
+        public string Item { get; set; }
+        public int Number { get; set; }
+        public bool PaymentConfirmed { get; set; }
+        public bool Delivered { get; set; }
+    }
+}
